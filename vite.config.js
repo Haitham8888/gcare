f53565b779f233d4f0f962d0efa5caaa5e20e3ad
Haitham8888 <hattan133@gmail.com>
2026-02-04 19:26:01 +0000

    Updates

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import solid from 'vite-plugin-solid'
-
-const isProd = process.env.NODE_ENV === 'production'
-
-export default defineConfig({
-  base: '/',
-  plugins: [solid()],
-})
